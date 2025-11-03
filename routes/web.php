c0d7c7a35599796d19651431c13d1018f57a197b
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2025-11-03 12:14:43 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,9 @@ Route::get('test', function () {
     return 'Test Abdian';
 });
 
+Route::get('teste', function () {
+    return 'test test';
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
